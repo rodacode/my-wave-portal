67f94383e552837cd79ee08fa90a3b34a453d397
nicohardmeier <nicolas.hardmeier@yahoo.es>
2021-10-17 17:52:38 +0200

    add chakra ui

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
 import * as React from "react";
 import { ethers } from "ethers";
 import './App.css';
+import { ChakraProvider } from "@chakra-ui/react"
+
+import { Button, Text } from '@chakra-ui/react'
 
 export default function App() {
 
   const wave = () => {
-    
+
   }
-  
+
   return (
+    <ChakraProvider>
+
     <div className="mainContainer">
 
       <div className="dataContainer">
         <div className="header">
-        <h1>👋 Hey there!</h1>
+          <Text
+            bgGradient="linear(to-l, #7928CA, #FF0080)"
+            bgClip="text"
+            fontSize="2xl"
+            fontWeight="extrabold"
+            className="title"
+            textAlign="center">Welcome to my</Text>
+                      <Text
+            bgGradient="linear(to-l, #7928CA, #FF0080)"
+            bgClip="text"
+            fontSize="6xl"
+            fontWeight="extrabold"
+            className="title"
+            textAlign="center">👋 Wave Portal</Text>
+            
         </div>
 
         <div className="bio">
-            Hi I'm Nico and I like a lot cheese and potatoes, and also cheese.
+          Hi I'm Nico and I like cheese and potatoes, and also cheese.
         </div>
 
-        <button className="waveButton" onClick={wave}>
+        <Button className="waveButton" onClick={wave}>
           Wave at Me
-        </button>
+        </Button>
       </div>
     </div>
+    </ChakraProvider>
+
   );
 }
